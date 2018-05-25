f7d44d38c8c6747464f85ef5ff94f5df9475f107
Nick <barmatun101@mail.ru>
2018-05-25 21:24:41 +0300

    Fixes
    Пофикшен флексбокс афишы, ссылки .33, размеры шрифтов в переменных, чего-то там еще

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
 	sass = require('gulp-sass');
 	browserSync = require('browser-sync');
-
+	prefix = require('gulp-autoprefixer')
 
 gulp.task('sass', function() {
 	return gulp.src(['docs/sass/**/*.scss', 'docs/sass/**/*.sass'])
 	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(prefix('last 2 versions'))
 	.pipe(gulp.dest('docs/css'))
 	.pipe(browserSync.reload({stream: true}))
 });
-
 
 gulp.task('browser-sync', function() {
 	browserSync({
@@ -24,6 +24,7 @@ gulp.task('watch', ['browser-sync', 'sass'], function() {
 	gulp.watch('docs/sass/**/*.scss', ['sass']);
 	gulp.watch('docs/*.html', browserSync.reload);
 	gulp.watch('docs/js/**/*.js', browserSync.reload);
+
 });
 
 gulp.task('default', ['watch']);
