dc3b685fc719077dc121d7fa4fb9d06b59d7e91a
Nick <barmatun101@mail.ru>
2018-07-19 21:33:11 +0300

    About page

--- header.php
+++ header.php
@@ -6,22 +6,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-  <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="img/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/img/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/img/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/favicon-16x16.png">
   <link rel="manifest" href="/manifest.json">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="img/ms-icon-144x144.png"> <meta name="theme-color" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/img/ms-icon-144x144.png"> <meta name="theme-color" content="#ffffff">
 
   <title><?php wp_title('—', true, 'right'); ?> <?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/lib/fontawesome-all.min.css">
--- page-about.php
+++ page-about.php
@@ -3,10 +3,10 @@
 Template Name: about
 */
 get_header(); ?>
-	<!-- Контент -->
+<!-- Контент -->
 
-	<!-- Анонс -->
-	<div class="page-content">
+<!-- Анонс -->
+<div class="page-content">
 
 	<section class="org-sostav wrapper">
 		<h2 class="letter-spacing-300">Организаторы</h2>
@@ -16,20 +16,274 @@ get_header(); ?>
 			<li class="org-sostav__one-face"><img src="<?php echo get_template_directory_uri(); ?>/img/tamara.png" alt="Семёнова" class="org-sostav__photo"><p>Семёнова<br> Тамара Гавриловна<span class="org-sostav__found-role">основатель</span></p></li>
 			<li class="org-sostav__one-face"><img src="<?php echo get_template_directory_uri(); ?>/img/azat.png" alt="Мамединов" class="org-sostav__photo"><p>Мамединов<br> Азат Аббасович<span class="org-sostav__found-role">основатель</span></p></li>
 		</ul>
-		<div class="org-sostav__pop-sovet">попечительский совет
-			<ul class="ors-sostav__sovet-list">
-				<li class="org-sostav__role">4</li>
-				<li class="org-sostav__role">4</li>
-				<li class="org-sostav__role">4</li>
-				<li class="org-sostav__role">4</li>
-				<li class="org-sostav__role">4</li>
-				<li class="org-sostav__role">4</li>
-				<li class="org-sostav__role">4</li>
-				<li class="org-sostav__role">4</li>
+		<div class="org-sostav__pop-sovet"><h2 class="about-date-title">попечительский совет</h2> 
+			<ul class="org-sostav__sovet-list">
+				<li class="org-sostav__role">Пиотровский Михаил Борисович <span>генеральный директор Государственного Эрмитажа</span></li>
+				<li class="org-sostav__role">Говорухин Станислав Сергеевич <span>художественный руководитель киностудии «Вертикаль»</span></li>
+				<li class="org-sostav__role">Гусев Владимир Александрович <span>директор Государственного Русского музея</span></li>
+				<li class="org-sostav__role">Церетели Зураб Константинович <span>президент Академии художеств России</span></li>
+				<li class="org-sostav__role">Русу Игорь Михайлович <span>генеральный директор ООО РПМ «Тамань»</span></li>
+				<li class="org-sostav__role">Мельникова Анастасия Рюриковна <span>заслуженный артист Российской Федерации</span></li>
+				<li class="org-sostav__role">Мамединов Азат Аббасович <span>автор и продюсер проекта Международный фестиваль искусств «МАСТЕР КЛАСС»</span></li>
+				<li class="org-sostav__role">Чвыр Илья Хасанович <span>президент Международного благотворительного фонда «Поощрение искусств»</span></li>
 			</ul>
 		</div>
 	</section>
 
+	<section class="fond-about1993 wrapper">
+		<h2>Фонд «Мастер Класс» основан в 1993-м году.</h2>
+		<p>Некоммерческая организация Международный фонд поддержки культуры «МАСТЕР КЛАСС», проводит Международtный фестиваль искусств «МАСТЕР КЛАСС» ежегодно в Санкт-Петербурге с 1993 года. </p>
+		<p>Фонд «МАСТЕР КЛАСС» провел десятки выставок, фестивалей и культурных акций. Самые известные из них: «Дети рисуют в храме», «Трава на дворцовой площади», «Эра милосердия», «Благотворительный Северный бал».</p>
+		<p>Цели Международного фестиваля искусств «МАСТЕР КЛАСС»: объединение творческих сил, просветительство, пропаганда основ классического и современного искусств. Мы представляем культуру Санкт-Петербурга в регионах России и за рубежом, знакомим петербуржцев с мировой культурой. </p>
+		<p>Фонд поддерживает молодых талантливых художников, дизайнеров, литераторов и музыкантов. Многие из них смогли донести свое творчество до жителей Петербурга и стали известными.</p>
+		<p>Работа фонда построена на связи поколений. Мастера делятся опытом с молодежью. Юноши и девушки получают опыт на практике во время мастер-классов, а не через скучные теоретические лекции.</p>
+		<p>Фонд «МАСТЕР КЛАСС» призван искать новые формы и направления в искусстве, сохранять старые классические традиции, основываться на профессиональной базе Высшей школы. </p>
+	</section>
+	
+	<div class="about-targs wrapper">
+		<div class="about-targs__div">
+			<h2 class="about-date-title">цель</h2>
+			<p>Приобщение подрастающего поколения к изучению культуры Санкт-Петербурга и мировой культуры, использованию ее лучших достижений для приобретения опыта и практических знаний.</p>
+		</div>
+		<div class="about-targs__div">
+			<h2 class="about-date-title">принципы</h2>
+			<ol>
+				<li>Фестиваль призван пропагандировать
+				культуру России.</li>
+				<li>В проекте участвуют профессиональные художники, музыканты, архитекторы, кинематографисты, театральные деятели, писатели, журналисты.</li>
+			</ol>
+		</div>
 	</div>
+
+	<section class="history wrapper">
+		<h2 class="letter-spacing-300">ИСТОРИЯ</h2>
+		
+		<div class="history__year-item">
+			<span class="history__year">1993</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>Состоялась премьера фестиваля, посвященного теме экологии культуры и души человека — «Экология мира». Это первое выступление 16 художников положило начало новому творческому объединению в Cанкт-Петербурге, сообществу профессионалов пропагандирующих принципы передачи творческого опыта и знаний от учителя к ученику в форме Мастер-классов.</p>
+						<ul class="history__photos">
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1993.1.jpg" data-lightbox="history1"><img src="<?php echo get_template_directory_uri(); ?>/img/history1993.1.jpg" alt="1"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1993.2.jpg" data-lightbox="history1"><img src="<?php echo get_template_directory_uri(); ?>/img/history1993.2.jpg" alt="2"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1993.3.jpg" data-lightbox="history1"><img src="<?php echo get_template_directory_uri(); ?>/img/history1993.3.jpg" alt="3"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1993.4.jpg" data-lightbox="history1"><img src="<?php echo get_template_directory_uri(); ?>/img/history1993.4.jpg" alt="4"></a>
+								<span class="history__photos-more"><span class="num">8</span><br>фото</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">1994</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>В Санкт-Петербурге прошли Игры Доброй Воли. Помимо освещения спортивных мероприятий телеканалом «Останкино» ежедневно транслировались репортажи  с Международного фестиваля искусств «МАСТЕР КЛАСС». Над темой «Город и незнакомка» работали 150 художников. Известный тенор Лучано Паваротти представил для экспозиции в Аничковом Дворце несколько авторских литографий.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">1995</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>Фестиваль прошел под девизом «Художник и модель» и был приурочен к открытию реконструированных фонтанов в Петергофе. Гала-церемония   состоялась 12 июля на фонтане «Большого каскада», фестиваль «МАСТЕР КЛАСС» принимал 200 участников.</p>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">1996</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>В июне в Санкт-Петербурге прошел Месяц Европейской Культуры. Тема фестиваля — «Похищение Европы». В этом году впервые в истории фестиваля «МАСТЕР КЛАССА» номинантам присуждается звание «Мастер» и вручается почетный знак — синяя бархатная шапочка «Мастер». Церемония закрытия прошла на ступенях Инженерного замка.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">1997</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>Фестиваль этого года стал продолжением исследования петербургской темы  «Сны и иллюзии Санкт-Петербурга» (из рукописей Н. Гумилева). В программу фестиваля были включены фото — мастер-класс, кинопросмотр, экспозиция произведений современной монументальной скульптуры, организованная на площади Островского, возле памятника Екатерине II. Заключительная галла-церемония прошла в Академическом Александрийском театре. Совет Мастеров вручил дипломы «МАСТЕР» Андрею Мыльникову, Валерию Гаврилину, Никите Долгушину, Равилю Мартынову.</p>
+						<ul class="history__photos">
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1997.1.jpg"  data-lightbox="history2" ><img src="<?php echo get_template_directory_uri(); ?>/img/history1997.1.jpg" alt="1"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1997.2.jpg"  data-lightbox="history2" ><img src="<?php echo get_template_directory_uri(); ?>/img/history1997.2.jpg" alt="2"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1997.3.jpg"  data-lightbox="history2" ><img src="<?php echo get_template_directory_uri(); ?>/img/history1997.3.jpg" alt="3"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history1997.4.jpg"  data-lightbox="history2" ><img src="<?php echo get_template_directory_uri(); ?>/img/history1997.4.jpg" alt="4"></a>
+								<span class="history__photos-more"><span class="num">35</span><br>фото</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">1998</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>Русскому музею 100-лет со дня своего открытия. Участники фестиваля — работали над темой, заданной Русским музеем, — «Диалог столетий. Разговор Казимира Малевича и Андрея Рублева». Музыкальную программу фестиваля представили лауреат международных конкурсов, пианист Владимир Мищук, народная артистка СССР, скрипачка и симфонический дирижер Лиана Исакадзе, ведущая солистка Женевской Оперы, Английской Национальной Оперы, Ковент Гарден и Гранд Опера, Линда Финни. В рамках фестиваля на Малой Конюшенной улице и в Летнем Саду прошла выставка городской скульптуры. Заключительный гала-концерт прошел в Михайловском саду, на парадной лестнице садового фасада Русского музея. В гала-концерте принимали участие Государственный Академический Театр под руководством Бориса Эйфмана, хор под руководством Елизаветы Кудрявцевой. В церемонии приняла участие Наталия Дудинская. Санкт-Петербургский Государственный симфонический оркестр под управлением Равиля Мартынова, Хор мальчиков Санкт-Петербурга под управлением Пчелкина.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">1999</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>Флаг фестиваля «МАСТЕР КЛАСС» поднялся в честь А.С. Пушкина. Около 120 художников, скульпторов, фотографов из 15 стран мира работали над темой, заданной Институтом русской литературы РАН (Пушкинским домом), к 200-летию со дня рождению поэта - «Поэт сам избирает предметы для своих песен, толпа не имеет право управлять его вдохновением…»,(А.С. Пушкин «Египетские ночи»). Почетными гостями фестиваля в этом году стали Станислав Говорухин, Елена Образцова, Никита Михалков, а также участники Экономического форума Межпарламентской ассамблеи государств-участников СНГ. Торжественная гала-церемония состоялась перед храмом Воскресения Христова («Спас-на-Крови»).</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">2000</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month"></span>
+					<div class="history__event">
+						<p>Тему фестивалю предоставляет директор Государственного Эрмитажа Михаил Пиотровский. Художники создают произведения на тему «Эрмитаж». Гала- концерт проходит во дворе Зимнего Дворца впервые за 70 лет, перед этим расчищают от строительного мусора этот двор. В доме кино целый месяц работает фото-выставка и просмотр кинолент известных режиссеров в Доме Архитектора прошли семинары и дискуссии на тему современной Архитектуры.</p>
+					</div>
+				</div>
+				<div class="history__month-event">
+					<span class="history__month">октябрь</span>
+					<div class="history__event">
+						<p>Масштабная акция фестиваля «МАСТЕР КЛАСС» в Центральной Азии. Из 16 стран мира в Алма-Ату прибыли художники, чтобы провести Международный фестиваль искусств «МАСТЕР КЛАСС» в Центральном музее Казахстана.</p>
+					</div>
+				</div>
+				<div class="history__month-event">
+					<span class="history__month">октябрь</span>
+					<div class="history__event">
+						<p>Совместно с правительством Мексики Международный фестиваль искусств «МАСТЕР КЛАСС» впервые проводит конкурс «Мексика глазами российских художников». Два гранда из трех получают участники фестиваля Вера Лихачева и Эрвин Виленский.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">2001</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month">с 19 марта по 20 апреля</span>
+					<div class="history__event">
+						<p>Международным фестивалем искусств «МАСТЕР КЛАСС» совместно с Центральным Выставочным залом Манеж была проведена масштабная Городская выставка «Мир отражений», в которой участвовали самые яркие проекты последнего десятилетия: Новая Академия Изящных Искусств, Малая Академия, театр рок-оперы, объединенная писательская группа современных писателей, редакция журнала "Мир металла" и другие. Выставка прошла с огромным успехом, была отмечена Российским Институтом искусствознания, как яркое явление нового тысячелетия и широко освещалась центральными и Санкт-Петербургскими средствами массовой информации.</p>
+					</div>
+				</div>
+
+				<div class="history__month-event marginBottom0">
+					<span class="history__month">6–14 июня</span>
+					<div class="history__event">
+						<p>Прошел самый народный фестиваль «МАСТЕР КЛАСС» за все 9 лет были проведены 4 масштабных уличных акции:</p>
+					</div>
+				</div>
+
+				<div class="history__month-event marginBottom0">
+					<span class="history__month">7 июня</span>
+					<div class="history__event">
+						<p>«Мой современник» на площади Искусств;</p>
+					</div>
+				</div>
+
+				<div class="history__month-event marginBottom0">
+					<span class="history__month">9 июня</span>
+					<div class="history__event">
+						<p>на Дворцовой площади — конкурс детского рисунка 
+						«Путешествие из Ленинграда в Санкт-Петербург»;</p>
+					</div>
+				</div>
+
+				<div class="history__month-event marginBottom0">
+					<span class="history__month">10 июня</span>
+					<div class="history__event">
+						<p>арт-терапия на Манежной площади;</p>
+					</div>
+				</div>
+
+				<div class="history__month-event">
+					<span class="history__month">12 июня</span>
+					<div class="history__event">
+						<p>Мастер-класс кузнецов — на Манежной площади.</p>
+					</div>
+				</div>
+
+				<div class="history__months-events">
+					<div class="history__month-event">
+						<span class="history__month">20 июня</span>
+						<div class="history__event">
+							<p>Церемония вручения знака и звания «МАСТЕР» прошла в Государственном Академическом Капелле Санкт-Петербурга. </p>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+
+		<div class="history__year-item">
+			<span class="history__year">2002</span>
+			<div class="history__months-events">
+				<div class="history__month-event">
+					<span class="history__month">6–23 июня</span>
+					<div class="history__event">
+						<p>Масштабная выставка в Центральном выставочном зале «Манеж», тема «Арт Балтика». Конкурс скульптуры из вторичного сырья-мусора, обширная музыкальная программа. В рамках фестиваля были проведены:</p>
+					</div>
+				</div>
+
+				<div class="history__month-event">
+					<span class="history__month">9 июня</span>
+					<div class="history__event">
+						<p>детский праздник на Дворцовой площади;</p>
+					</div>
+				</div>
+
+				<div class="history__month-event">
+					<span class="history__month">11 июня</span>
+					<div class="history__event">
+						<p>в Гранд-Отеле «Европа» был проведен Мастер-класс лауреата премии «Оскар» художника-аниматора Александра Петрова, который получил высшую мировую награду мира кино за свой мультфильм «Старик и море»;</p>
+					</div>
+				</div>
+
+				<div class="history__month-event">
+					<span class="history__month">23 июня</span>
+					<div class="history__event">
+						<p>Заключительная гала-церемония фестиваля «МАСТЕР-КЛАСС» проходила на Невском проспекте в клубе «Holliwood Night». Знак и звания «Мастер» вручена Вячеславу Бутусову. </p>
+						<ul class="history__photos">
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history2002.1.jpg"  data-lightbox="history3" ><img src="<?php echo get_template_directory_uri(); ?>/img/history2002.1.jpg" alt="1"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history2002.2.jpg"  data-lightbox="history3" ><img src="<?php echo get_template_directory_uri(); ?>/img/history2002.2.jpg" alt="2"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history2002.3.jpg"  data-lightbox="history3" ><img src="<?php echo get_template_directory_uri(); ?>/img/history2002.3.jpg" alt="3"></a></li>
+							<li><a href="<?php echo get_template_directory_uri(); ?>/img/history2002.4.jpg"  data-lightbox="history3" ><img src="<?php echo get_template_directory_uri(); ?>/img/history2002.4.jpg" alt="4"></a>
+								<span class="history__photos-more"><span class="num">24</span><br>фото</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</div>
 
 <?php get_footer(); ?>
